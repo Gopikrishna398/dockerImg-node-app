4237689fb85497fb6cc9355c75b95c2c3f3fae84
Chippalapalli Gopi Krishna <gopikrishnachippalapalli@gmail.com>
2026-02-28 22:43:44 +0530

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.end("Hello Gopi Krish! Docker Web App Running 🚀");
+    res.end("Hello Gopi Krishaaa! Docker Web App Running 🚀");
 });
 
 server.listen(3000, () => {
